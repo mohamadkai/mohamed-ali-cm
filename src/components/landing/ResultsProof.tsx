b66b4e77c402fb912931cc9f1fe011ce3fe95cc5
mohamed ali <mohamadaly990@gmail.com>
2026-05-01 19:07:23 +0300

    Delete src/components/landing/ResultsProof.tsx

--- src/components/landing/ResultsProof.tsx
+++
@@ -1,9 +0,0 @@
-export default function ResultsProof() {
-  return (
-    <section className="px-4 py-20 text-white bg-red-500">
-      <h2 className="text-5xl font-black text-center">
-        TEST RESULTS SECTION
-      </h2>
-    </section>
-  );
-}
